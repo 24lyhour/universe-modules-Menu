a21ad2f788588d29e93d7bc1d7d269b713c82ced
Lyhour <kouchlyhour@gmail.com>
2026-02-11 15:33:24 +0700

    feat: add menu item registration for the Menu module.

---
+++ resources/js/pages/Types/Menu.ts
@@ -0,0 +1,15 @@
+
+
+/**
+ * for the index menu
+ */
+
+export interface Menu {
+    id: number;
+    name: string;
+    url: string;
+    icon: string;
+    order: number;
+    permissions: string;
+    route: string;
+}
--- resources/js/pages/dashboard/Menu/Index.vue
+++ resources/js/pages/dashboard/Menu/Index.vue
@@ -1,6 +1,18 @@
 <template>
-    hello world
-</template>
-<script setup>
+     <AppLayout >
 
+        <div> 
+            hello
+        </div>
+     </AppLayout>
+</template>
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+
+// const breadcrumbs = [
+//     {
+//         title: 'Menu',
+//         url: route('menu.index'),
+//     },
+// ];
 </script>
